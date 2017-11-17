1f69a92e01912b24c7e82f96b10263339c91b2b9
Tohir <thohiru.omoloye@talentbase.ng>
2017-11-17 00:33:08 +0000

    added resource route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,10 @@
 
 const noteRoutes = require('./note_routes')
 const accountRoutes = require('./account_routes')
+const resourceRoutes = require('./resource_routes')
 
 module.exports = function(app, db){
     noteRoutes(app, db)
     accountRoutes(app, db)
+    resourceRoutes(app, db)
 }
---
+++ app/routes/resource_routes.js
@@ -0,0 +1,7 @@
+// routes/resource_routes.js
+module.exports = function(app, db) {
+    app.get('/resource/countries', (req, res) => {
+        console.log('coming soon')
+        res.send('coming soon')
+    });
+};
